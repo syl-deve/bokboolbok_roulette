26ac3293c8e241d7659d92e32d9149fd9b5a2064
malee <mythe627@naver.com>
2026-02-15 12:24:41 +0900

    ﻿MultiDex 활성화 및 INTERNET 권한 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     signingConfigs {
