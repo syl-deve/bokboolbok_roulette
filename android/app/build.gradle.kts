3efd0d0c13dd3033c0efc9f0a6fcadc72acf2d36
malee <mythe627@naver.com>
2026-07-24 20:05:24 +0900

    Update Android release policy requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 android {
     namespace = "com.malee.bokboolbok_roulette"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973" // ✅ 최신 NDK
 
     compileOptions {
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         applicationId = "com.malee.bokboolbok_roulette"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
